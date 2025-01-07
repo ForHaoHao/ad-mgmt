cd6847eb8f1c34b9b73344a23e64c3c56d41018d
Hao.chen <hao.chen@patriotmemory.com>
2025-01-07 17:46:12 +0800

    feat: First commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"ADMgmtSystem/library"
+	"ADMgmtSystem/routers"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	router := routers.NewRouter()
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	port := os.Getenv("HTTP_PORT")
+	domain := os.Getenv("HTTP_DOMAIN")
+	portocol := os.Getenv("HTTP_PORTOCOL")
+
+	library.OpenBrowser(fmt.Sprintf("%s://%s:%s", portocol, domain, port))
+
+	router.Run(fmt.Sprintf("%s:%s", domain, port))
+}
